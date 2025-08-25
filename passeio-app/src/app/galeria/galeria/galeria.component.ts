f91f11570c072e03450e4c8bbc9f5f113bf9f3d8
Thuany Mendonca <thuany1997@hotmail.com>
2025-08-24 22:30:45 -0300

    Adicionando a tela de galeria que faz pesquisa de lugares com foto e detalhes

--- passeio-app/src/app/galeria/galeria/galeria.component.ts
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { Categoria } from '../../categorias/categoria';
+import { Lugar } from '../../lugares/lugar';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +10,32 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss'
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
 
+  lugares: Lugar[] = [];
+  categoriasFiltro: Categoria[] = [];
+  nomeFiltro: string = '';
+  categoriaFiltro: string = '';
+
+  constructor(private lugarService: LugarService, private categoriaService: CategoriaService) { }
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe(categorias => this.categoriasFiltro = categorias);
+    // ou assim
+    this.lugarService.obterTodos().subscribe({
+      next: (lugaresResposta) => {
+        this.lugares = lugaresResposta
+        console.log('Lugares: ', this.lugares);
+      },
+      error: (erro) => console.error('Erro ao listar lugares: ', erro.message)
+    });
+  }
+
+  getTotalEstrelas(lugar: Lugar) : string {
+    return '&#9733;'.repeat(lugar.avaliacao || 0) + '&#9734;'.repeat(5 - (lugar.avaliacao || 0));
+  }
+
+  filtrar(){
+    this.lugarService.filtrar(this.nomeFiltro, this.categoriaFiltro).subscribe(resultado => this.lugares = resultado)
+  }
 }
